a5b63f077a2eb7428ec6310f070317f13427d828
Aman Wats <aman.wats@outlook.com>
2021-04-11 15:38:16 +0530

    created header component

---
+++ amanzon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCz1yV2eRkU9D4OwUNgCRWq93yRfS0JyIo',
+  authDomain: 'challenge-5c050.firebaseapp.com',
+  projectId: 'challenge-5c050',
+  storageBucket: 'challenge-5c050.appspot.com',
+  messagingSenderId: '414440581489',
+  appId: '1:414440581489:web:1013a193c9db1fb54a2ed0',
+  measurementId: 'G-ZN7MJTD398',
+};

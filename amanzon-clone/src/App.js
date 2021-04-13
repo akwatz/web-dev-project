2fbf2dc74e34d04074d1397c0e40fe88de2970f3
Aman Wats <aman.wats@outlook.com>
2021-04-13 12:51:14 +0530

    end of day 1

--- amanzon-clone/src/App.js
+++ amanzon-clone/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-
+import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 function App() {
   return (
-    <div className='app'>
-      <h1>Hello</h1>
+    <Router>
       <Header />
-    </div>
+      <div className='app'>
+        <Switch>
+          <Route path='/checkout'>
+            <Checkout />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- amanzon-clone/src/Header.js
+++ amanzon-clone/src/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBaketIconIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
   return (
@@ -9,7 +11,10 @@ function Header() {
         src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
       />
 
-      <div className='header__search'></div>
+      <div className='header__search'>
+        <input className='header__searchInput' type='text' />
+        <SearchIcon className='header__searchIcon' />
+      </div>
 
       <div className='header__nav'>
         <div className='header__option'>
@@ -26,6 +31,11 @@ function Header() {
           <span className='header__optionLineOne'>Your</span>
           <span className='header__optionLineTwo'>Prime</span>
         </div>
+      </div>
+
+      <div className='header__optionBasket'>
+        <ShoppingBaketIconIcon />
+        <span className='header__optionLineOne header__basketCount'>0</span>
       </div>
     </div>
   );
